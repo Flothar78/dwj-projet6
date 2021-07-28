9e75e46f586079b44a861542935b2adf93ea93a4
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-07-28 16:18:47 +0200

    connexion et modèles mongoose OK

---
+++ backend/models/user.js
@@ -0,0 +1,6 @@
+const userSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+module.exports = mongoose.model("User", userSchema);
