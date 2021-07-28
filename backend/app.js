17bfc7be18dadaf5762fc982a05e753bc8d0fc37
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-07-28 14:38:33 +0200

    routes POST et GET OK

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,53 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   next();
 });
 
-app.use("/api/auth/signup", (req, res, next) => {
-  const inscription = [
-    { email: "jeanedgar@gonit.org", password: "ABCDEFRTGH" },
+app.use(bodyParser.json());
+
+app.post("/api/auth/signup", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post("/api/auth/login", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Login OK",
+  });
+  next();
+});
+
+app.use("/api/sauces", (rep, res, next) => {
+  const sauces = [
+    {
+      userId: "12a31b2de31153123a318861",
+      name: "Sauce Bleue",
+    },
+    {
+      userId: "12a31b2de312f23fd23a318861",
+      name: "Sauce Rouge",
+    },
+    {
+      userId: "12a31b2de312f2d123a318861",
+      name: "Sauce Jaune",
+    },
+    { userId: "12a31b2de312ac48123a318861", name: "Sauce Verte" },
   ];
-  res.status(200).json(inscription);
+  res.status(202).json(sauces);
+  next();
 });
 
 module.exports = app;
