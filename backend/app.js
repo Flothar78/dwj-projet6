2f77fc8aaeaa2ecb92aef56e30dbf00d598e52b7
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-08-06 14:50:57 +0200

    routes sauces avant modif structure

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const multer = require("./middleware/multer-config.js");
 
 const cors = require("cors");
 const app = express();
@@ -35,5 +36,45 @@ app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api/auth", userRoutes);
+
+app.post("/api/sauces", multer, (req, res, next) => {
+  console.log(req.body);
+  let content = JSON.parse(req.body.sauce);
+  const sauce = new Sauce({
+    userId: "toto",
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+    ...content,
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json(req.body))
+    .catch((error) => res.status(402).json({ error }));
+});
+
+app.put("/api/sauces/:id", (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/sauces/:id", (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.use("/api/sauces", (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(207).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+});
 
 module.exports = app;
